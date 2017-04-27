8679babdbbebb42f80d1822eec160aba64733c5e
SintijaK <B00684209@scm.net>
2017-04-27 22:54:33 +0100

    files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Sports Equipment 1.0
+                    Sports Equipment
                 </div>
 
                 <div class="links">
